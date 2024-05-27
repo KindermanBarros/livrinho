1f44e58813c03bb55426e3fadd1c58ed74e71940
Kinderman <kindbarros@gmail.com>
2024-05-27 08:41:48 -0400

    Testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,13 +8,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin:['https://main.d3h5skblfcr1h9.amplifyapp.com/']}));
 app.use(express.json());
 
 const port = process.env.PORT || 3001;
 const googleApiKey = process.env.GOOGLE_API_KEY;
 
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 if (!googleApiKey) {
   console.error('Environment variable GOOGLE_API_KEY must be set.');
